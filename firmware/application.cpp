d4f10f37b9d9590b419048e884f593249302a811
John Wass <jwass3@gmail.com>
2018-12-30 10:46:53 -0500

    initial sketch of temp sensing

--- firmware/application.cpp
+++ firmware/application.cpp
@@ -1,8 +1,21 @@
 #include <Particle.h>
+#include <OneWire.h>
+#include <spark-dallas-temperature.h>
+
+long previous = 0;
+long interval = 5 * 1000;
+
+OneWire w1(D4);
+DallasTemperature sensor(&w1);
 
 void setup() {
+   sensor.begin();
 }
 
-
 void loop() {
+   if(millis() - previous > interval) {
+      sensor.requestTemperatures();
+      String s(sensor.getTempFByIndex(0));
+      previous = millis();
+   }
 }
