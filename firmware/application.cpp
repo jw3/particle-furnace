48b7819435365f2b5ca333e1a7a9fccb6f868d8d
John Wass <jwass3@gmail.com>
2019-01-07 21:11:50 -0500

    temp reads and fails with history

--- firmware/application.cpp
+++ firmware/application.cpp
@@ -2,31 +2,37 @@
 #include <OneWire.h>
 #include <spark-dallas-temperature.h>
 
+#include <db.hpp>
 #include <pl.hpp>
 #include <bus.hpp>
 #include <events.hpp>
 
-using EventBus = eventpp::Bus<TempRead>;
+using EventBus = eventpp::Bus<TempRead, TempReadFail>;
+using TempBuffer = Buffer<TempRead>;
 
-long previous = 0;
-long interval = 5 * 1000;
+system_tick_t previous = 0;
+system_tick_t interval = 5 * 1000;
 
 OneWire w1(D4);
 DallasTemperature sensor(&w1);
 
 EventBus eb;
+auto tb = std::make_shared<TempBuffer>(100);
 ParticleListener pl;
 
 void setup() {
+   eb.add<TempRead>(tb);
    sensor.begin();
 }
 
 void loop() {
    if(millis() - previous > interval) {
       sensor.requestTemperatures();
-      sensor.getTempFByIndex(0);
-      previous = millis();
+      const auto val = sensor.getTempFByIndex(0);
+      if(val != DEVICE_DISCONNECTED_F)
+         eb.publish<TempRead>(static_cast<uint8_t>(val));
+      else eb.publish<TempReadFail>();
 
-      eb.publish<TempRead>();
+      previous = millis();
    }
 }
--- firmware/events.hpp
+++ firmware/events.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
 #include <event.hpp>
+#include <stdint.h>
 
-struct TempRead : public eventpp::Event<TempRead> {};
+struct TempRead : public eventpp::Event<TempRead>
+{
+   explicit TempRead(uint8_t val) : val(val) {};
+   uint8_t value() { return val; }
+private:
+   uint8_t val;
+};
+struct TempReadFail : public eventpp::Event<TempReadFail> {};
