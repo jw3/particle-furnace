a0705b538b6a5b5cebf3804dbf986e4e89e23a5a
John Wass <jwass3@gmail.com>
2019-01-08 23:55:52 -0500

    tick is passed in

--- firmware/fsm.cpp
+++ firmware/fsm.cpp
@@ -1,9 +1,6 @@
 #include <fsm.hpp>
 #include <events.hpp>
-
 #include <stdint.h>
-#include <spark_wiring_ticks.h>
-
 
 Behavior::OptBehavior Idle::operator()(const TempRead& temp, uint32_t tick) {
    if(temp.value() <= cfg.low())
@@ -14,7 +11,7 @@ Behavior::OptBehavior Idle::operator()(const TempRead& temp, uint32_t tick) {
 Behavior::OptBehavior Blowing::operator()(const TempRead& temp, uint32_t tick) {
    if(tempOff && tempOff.value() >= temp.value())
       return Idle(cfg);
-   if(millis() > tickOff)
+   if(tick > tickOff)
       return Idle(cfg);
    return SameBehavior;
 }
