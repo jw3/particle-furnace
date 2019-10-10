3802a530520ab1d8eb43e64a6f0925efe9cf4367
John Wass <jwass3@gmail.com>
2019-10-09 23:47:14 -0400

    latest conan
    - managed patches
    - managed flasher

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ -z "$1" ]]; then echo "usage: build.sh <platform>"; exit 1; fi
+if [[ -z "$1" ]]; then echo "usage: $0 <platform>"; exit 1; fi
 
-readonly platform="$1"
-readonly builddir="build-$platform"
+readonly platform_arg="$1"
+cmake_args="-DPLATFORM=$platform_arg"
+readonly builddir="build-$platform_arg"
+
+readonly cross_compiler_root=${CROSS_COMPILER_ROOT:-/usr/local/gcc-arm}
+readonly compiler_major_version=$("${cross_compiler_root}/bin/arm-none-eabi-gcc" -dumpspecs | grep *version -A1 | tail -n1 | cut -d. -f1)
 
 if [[ "$2" != "quick" ]]; then
   rm -rf "$builddir"
@@ -13,5 +17,6 @@ fi
 if [[ ! -d "$builddir" ]]; then mkdir "$builddir"; fi
 
 cd "$builddir"
-cmake .. -DPLATFORM="$platform"
+conan install .. -s compiler.version="$compiler_major_version"
+cmake .. ${cmake_args}
 make -j1
