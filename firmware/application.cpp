c517b7147ce5e62c35d08fb5cace4ea33ef1502a
John Wass <jwass3@gmail.com>
2020-11-25 17:52:37 -0500

    add oled

--- firmware/application.cpp
+++ firmware/application.cpp
@@ -1,11 +1,25 @@
 #include <Particle.h>
 #include <OneWire.h>
 #include <spark-dallas-temperature.h>
+#include <Adafruit_SSD1306.h>
+
+SYSTEM_MODE(MANUAL)
 
 auto w1 = OneWire(D4);
 auto sensor = DallasTemperature(&w1);
 
+void updateDisplay(float f, Adafruit_SSD1306& display) {
+   display.clearDisplay();
+   display.setTextColor(WHITE);
+   display.setTextSize(1);
+   display.setCursor(0, 10);
+   display.printf("Temp: %f F", f);
+   display.display();
+}
+
+Adafruit_SSD1306 tft(128, 32);
 void setup() {
+   updateDisplay(0.0, tft);
    sensor.begin();
 }
 
@@ -18,7 +32,7 @@ void loop() {
       const auto val = sensor.getTempFByIndex(0);
       const system_tick_t t = millis();
       if(val != DEVICE_DISCONNECTED_F) {
-         // valid
+         updateDisplay(val, tft);
          last = millis();
       }
    }
